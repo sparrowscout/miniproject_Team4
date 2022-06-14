69351918da14409774b8ce149b3a480f98ceef33
Bo-oram <930802qhfka@gmail.com>
2022-06-14 13:54:02 +0900

    게시물 디테일 페이지 스타일변경

--- src/Detail.js
+++ src/Detail.js
@@ -1,6 +1,7 @@
 import React,{useState, useRef} from "react";
 import { useNavigate, useLocation, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import './style/board.css'
 
 const Detail = (props) => {
   let postList = useSelector((state) => state.post.data);
@@ -8,14 +9,40 @@ const Detail = (props) => {
   let items = !postList ? [] : postList;
   let postId = useParams();
 
+  const Emoji = (type) => {
+    return (
+      (type === "calmness" && "평온해") ||
+      (type === "exited" && "기분 최고!") ||
+      (type === "great" && "완전 좋아") ||
+      (type === "flutter" && "설레") ||
+      (type === "tired" && "피곤해") ||
+      (type === "stress" && "짜증나") ||
+      (type === "worry" && "걱정돼") ||
+      (type === "blue" && "우울해") ||
+      (type === "soso" && "그저 그래")
+    );
+  };
+
+
   return (
     <>
       {items.map((i, idx) => {
         return i.id == postId.id ? (
           <div key={idx}>
-            {i.email}
-            <div className={i.type + "Image imgArea"}></div>
-            {i.text}
+            <div className="modeArea">
+              <h3>{Emoji(i.type)}</h3>
+              <p className={i.type + "Image imgArea"}></p>
+            </div>
+            <div className="conBox">
+              <div className="dateArea">
+                <p>{i.email}</p>
+                <p>|</p>
+                <p>2022-06-14</p>
+              </div>
+              <div className="teaxArea">
+                {i.text}
+              </div>
+            </div>
           </div>
         ) : null;
       })}
