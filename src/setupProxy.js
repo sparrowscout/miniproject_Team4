d1baacea959105ba3ef8a884c0887a3eee9d350f
sparrowscout <sparrowscout.0000@gmail.com>
2022-06-15 13:45:51 +0900

    Update setupProxy.js
    setupProxy 최신버전으로 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 
+// 프록시설정
+
 module.exports = function(app) {
-    app.use(
-      '/',
-      createProxyMiddleware({
-        target: 'http://3.39.226.189',
-        changeOrigin: true,
-      })
-    );
-  };
+  app.use(
+    createProxyMiddleware('/api', {
+      target: 'http://3.39.226.189', 
+      changeOrigin: true,
+    })
+  )
+}
 
