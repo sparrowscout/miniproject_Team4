13bc8666e43bb20749082dd30d28356ddebb31f7
sparrowscout <sparrowscout.0000@gmail.com>
2022-06-15 13:46:48 +0900

    로그인&회원가입 페이지 (서버통신 확인)
    로그인&회원가입 페이지 fetch 수정
    (오류 캐치 수정필요)

--- src/Login.js
+++ src/Login.js
@@ -3,8 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const Login = () => {
-    const dispatch = useDispatch()
-    
+
     const navigate = useNavigate();
     const [Email, setEmail] = useState("")
     const [Password, setPassword] = useState("")
@@ -15,111 +14,86 @@ const Login = () => {
 
     const onPasswordHandler = (event) => {
         setPassword(event.currentTarget.value)
+
+    }
+
+    let data = {
+        email: Email,
+        password: Password
+    }
+
+
+    async function userLogin() {
+        await fetch('/api/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        }).then((response) => response.json())
+        .then((data) => console.log('성공:', data))
+        navigate("/")
+    }
+
+
+
+        return (
+            <Container>
+                <h1>Login</h1>
+                <LoginContainer>
+                    <input placeholder="email" onChange={onEmailHandler} /> <br />
+                    <input placeholder="password" type="password" onChange={onPasswordHandler} /> <br />
+                    <button type="submit" onClick={userLogin}>로그인</button>
+                </LoginContainer>
+                <Link to="/register" >회원가입 </Link>
+
+            </Container>
+        );
         
-    }
-
-
-
-
-    const userLogin = async () => {
-        try {
-
-            const currentUser = {
-                "email": Email,
-                "password": Password
-            }
-    
-            // axios.post("/users/register", currentUser)
-            // .then(response => { console.log(response)})
-            // .catch(error => { console.log(error)})
-
-            const reponse = await axios.post('/users/login', currentUser);
-            console.log(...reponse.data);
-            dispatch(addUser(...reponse.data));
-            // setCurrentUser(reponse.data);
-            navigate("/");
-
-        } catch(error) {
-            alert(error)
         }
-    };
-    // const userLogin = () => {
-    //     axios.get('http://localhost:5001/users',{
-    //         params: {
-    //             "email":Email,
-    //             "password":Password
-    //         }
-    //     })
-    //     .then((response) => {
-    //         const current_user = response.data
-    //         console.log(...current_user)
-    //         dispatch(addUser(...current_user))
-    //         setCurrentUser(...current_user)
-    //         console.log(currentUser)
-    //         history.push("/")
-    //     })
-    //     .catch(function (error) {
-    //         alert(error)
-    //     });
-    // }
-
-    return (
-        <Container>
-            <h1>Login</h1>
-            <LoginContainer>
-                <input placeholder="email" onChange={onEmailHandler}/> <br />
-                <input placeholder="password" type="password" onChange={onPasswordHandler}/> <br />
-                <button type="submit" onClick={userLogin}>로그인</button>
-            </LoginContainer>
-            <Link to="/register" >회원가입 </Link> 
-
-        </Container>
-    )
-}
-
-const Container = styled.div`
-border: 1px solid transparent;
-max-width: 480px;
-margin: 30px auto;
-background-color: white;
-border-radius: 10px;
-padding: 30px 30px 50px 30px;
-text-align: center;
+    const Container = styled.div`
+        border: 1px solid transparent;
+        max-width: 480px;
+        margin: 30px auto;
+        background-color: white;
+        border-radius: 10px;
+        padding: 30px 30px 50px 30px;
+        text-align: center;
 
 
-a{
-    font-size: 12px;
-}
-`;
+        a{
+            font-size: 12px;
+        }
+        `;
 
-const LoginContainer = styled.div`
-input {
-    padding: 5px;
-    width: 200px;
-    margin: 5px 0px;
-}
+    const LoginContainer = styled.div`
+        input {
+            padding: 5px;
+            width: 200px;
+            margin: 5px 0px;
+        }
 
-button {
-    background-color: #FFBD42;
-    color:#222;
-    padding: 10px;
-    border: 1px solid transparent;
-    border-radius: 5px;
-    width: 214px;
-    margin: 10px 0px;
+        button {
+            background-color: #FFBD42;
+            color:#222;
+            padding: 10px;
+            border: 1px solid transparent;
+            border-radius: 5px;
+            width: 214px;
+            margin: 10px 0px;
 
 
-    :hover {
-        background-color: #FFBD42;
-        color: white;
-    }
+            :hover {
+                background-color: #FFBD42;
+                color: white;
+            }
 
-    :active {
-        background-color: #F59300;
-    }
-}
-`;
+            :active {
+                background-color: #F59300;
+            }
+        }
+        `;
 
 
 
-export default Login;
+    export default Login;
