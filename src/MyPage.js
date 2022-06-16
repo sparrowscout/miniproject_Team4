47ba0b750a6d8ed8c332a01d2b011fdf82f90bf6
sparrowscout <sparrowscout.0000@gmail.com>
2022-06-16 22:54:49 +0900

    final
    final commit

--- src/MyPage.js
+++ src/MyPage.js
@@ -7,20 +7,27 @@ import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useMatch } from "react-router-dom";
 
 const MyPage = () => {
 let nickname = useSelector((state) => state.post.nickname);
 const params = useParams();
 const userNickname = nickname.nickname;
 
+const matchPost = useMatch('/mypage/posts') 
+const matchMood = useMatch('/mypage/moods')
+
+
     return (
         <MainContainer>
             <h1>{userNickname}'s page</h1>
             <Tabs>
-                <Tab>
+                <Tab isActive={matchPost != null}>
                     <Link to="posts"> my posts</Link>
                 </Tab>
-                <Tab>
+                <Tab isActive={matchMood != null}>
                     <Link to="moods"> my moods</Link>
                 </Tab>
             </Tabs>
@@ -44,21 +51,24 @@ gap: 10px;
 
 `;
 
+
 const Tab = styled.span`
 text-align: center;
 text-transform: uppercase;
-background-color: #FFEEB9 ;
+background-color: ${(props) =>
+    props.isActive ? '#FFBD42' : '#FFEEB9'};
 padding: 10px 0px;
 border-radius: 10px 10px 0px 0px;
 
-/* a{
-    padding: 10px 110px;
-} */
+
+
 :hover {
     background-color: #FFBD42;
     
     a{
-        color: white;
+    color: ${(props) =>
+    props.isActive ? '#222' : 'white'};
+        
         
     }
 }
